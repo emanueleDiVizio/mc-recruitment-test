811bc6e8c4e305b9339830d2fe6ac9e10e7891e9
Emanuele di Vizio <emadivizio@gmail.com>
2020-05-15 13:28:13 +0200

    extract constants

--- app/config/production.js
+++ app/config/production.js
@@ -1,1 +1,7 @@
-export default {}
+export default {
+  defaultTimerDelay: 1000,
+  defaultTimerTimeoutInSeconds: 20,
+  defaultShiftDurationInHours: 8,
+  defaultDateFormatString: 'dddd, MMMM Do YYYY, h:mm:ss a',
+  defaultNotificationIntervalInHours: 2,
+}
